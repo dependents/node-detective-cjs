ff03f20c39b9ff6682d94bc32220dfe16b92e93a
Joel Kemp <mrjoelkemp@gmail.com>
2015-06-27 15:05:03 -0400

    Use new node-source-walk for es6 support

--- test/test.js
+++ test/test.js
@@ -33,24 +33,29 @@ describe('detective-cjs', function() {
 
   it('returns the dependencies of a commonjs module', function() {
     var deps = detective('var a = require("./a");\n var b = require("./b");');
-    assert(deps.length === 2);
+    assert.equal(deps.length, 2);
+  });
+
+  it('supports es6', function() {
+    var deps = detective('const a = require("./a");\n let b = require("./b");');
+    assert.equal(deps.length, 2);
   });
 
   it('returns an empty list if there are no dependencies', function() {
     var deps = detective('1 + 1;');
-    assert(!deps.length);
+    assert.equal(deps.length, 0);
   });
 
   it('accepts an AST', function() {
     var deps = detective(ast);
-    assert(deps.length === 1);
-    assert(deps[0] === './a');
+    assert.equal(deps.length, 1);
+    assert.equal(deps[0], './a');
   });
 
   it('calls escodegen generate for non-literal require arguments (#1)', function() {
     sinon.spy(escodegen, 'generate');
     detective('var a = require("./foo" + "bar");');
-    assert(escodegen.generate.called);
+    assert.ok(escodegen.generate.called);
     escodegen.generate.restore();
   });
 });
