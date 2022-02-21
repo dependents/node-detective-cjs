2737efc7fbb483f4fc00debdfb1d7be97d15aed5
XhmikosR <xhmikosr@gmail.com>
2022-02-21 20:58:08 +0200

    Update all the things (#14)
    * Update all the things
    
    * add .editorconfig
    * enforce LF
    * switch to ESLint
    * switch to GitHub Actions
    * remove the unused sinon devDependency

--- test/test.js
+++ test/test.js
@@ -1,41 +1,44 @@
+/* eslint-env mocha */
+
+'use strict';
+
 const assert = require('assert');
-const detective = require('../');
-const sinon = require('sinon');
+const detective = require('../index.js');
 
-describe('detective-cjs', function() {
-  const ast = {
-    type: 'Program',
-    body: [{
-      type: 'VariableDeclaration',
-      declarations: [{
-        type: 'VariableDeclarator',
-        id: {
+const ast = {
+  type: 'Program',
+  body: [{
+    type: 'VariableDeclaration',
+    declarations: [{
+      type: 'VariableDeclarator',
+      id: {
+        type: 'Identifier',
+        name: 'a'
+      },
+      init: {
+        type: 'CallExpression',
+        callee: {
           type: 'Identifier',
-          name: 'a'
+          name: 'require'
         },
-        init: {
-          type: 'CallExpression',
-          callee: {
-            type: 'Identifier',
-            name: 'require'
-          },
-          arguments: [{
-            type: 'Literal',
-            value: './a',
-            raw: './a'
-          }]
-        }
-      }],
-      kind: 'var'
-    }]
-  };
+        arguments: [{
+          type: 'Literal',
+          value: './a',
+          raw: './a'
+        }]
+      }
+    }],
+    kind: 'var'
+  }]
+};
 
-  it('returns the dependencies of a commonjs module', function() {
+describe('detective-cjs', () => {
+  it('returns the dependencies of a commonjs module', () => {
     const deps = detective('var a = require("./a");\n var b = require("./b");');
     assert.equal(deps.length, 2);
   });
 
-  it('returns the dependencies of a main require cjs module', function() {
+  it('returns the dependencies of a main require cjs module', () => {
     const deps = detective('var a = require("./a");\n var b = require.main.require("./b");');
 
     assert.equal(deps[0], './a');
@@ -43,37 +46,37 @@ describe('detective-cjs', function() {
     assert.equal(deps.length, 2);
   });
 
-  it('does exclude requires based on variable values', function() {
+  it('does exclude requires based on variable values', () => {
     const deps = detective('var a = require("./a");\n var b = "foo" + ".js";;\n var c = require(b);');
 
     assert.equal(deps[0], './a');
     assert.equal(deps.length, 1);
   });
 
-  it('returns an empty list if there are no dependencies', function() {
+  it('returns an empty list if there are no dependencies', () => {
     const deps = detective('1 + 1;');
     assert.equal(deps.length, 0);
   });
 
-  it('accepts an AST', function() {
+  it('accepts an AST', () => {
     const deps = detective(ast);
     assert.equal(deps.length, 1);
     assert.equal(deps[0], './a');
   });
 
-  it('does not throw on jsx', function() {
-    assert.doesNotThrow(function() {
+  it('does not throw on jsx', () => {
+    assert.doesNotThrow(() => {
       detective('var a = require("./foobar"); var templ = <jsx />');
     });
   });
 
-  describe('es6', function() {
-    it('supports es6 syntax', function() {
+  describe('es6', () => {
+    it('supports es6 syntax', () => {
       const deps = detective('const a = require("./a");\n let b = require("./b");');
       assert.equal(deps.length, 2);
     });
 
-    it('supports template literals', function() {
+    it('supports template literals', () => {
       const deps = detective('const a = require("./a");\n let b = require("./b");\n var c = require(`./c`);');
       assert.equal(deps.length, 3);
     });
